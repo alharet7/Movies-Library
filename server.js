b7e0e81cea4d3b8c72bc8484b875e8120af893cc
alharet7 <hdhareth@gmail.com>
2023-05-02 08:15:27 +0000

    lab13 modified version

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const server = express();
-const data = require(`./Movie Data/data.json`)
+const data = require(`./MovieData/data.json`)
 
 const PORT = 3000;
 
-server.get(`/`,(req,res)=>{
-    const returnData ={
-        title: data.title,
-        poster_path:data.poster_path,
-        overview:data.overview,
+server.get(`/`, (req, res) => {
+   
+    const movie = new Movie(data.title, data.poster_path, data.overview)
+
+    function Movie(title, poster_path, overview) {
+        this.title = title
+        this.poster_path = poster_path
+        this.overview = overview
     }
-    res.send(returnData);
-    
+    res.send(movie);
+
 })
-server.get(`/favorite`,(req,res)=>{
+server.get(`/favorite`, (req, res) => {
     res.status(200).send('Welcome to Favorite Page')
 })
-server.get(`*`,(req,res)=>{
+server.get(`*`, (req, res) => {
     res.status(404).send('page not found')
 })
-server.use(function(err,req,res,text){
+server.use(function (err, req, res, text) {
     res.status(500).send('server error')
 })
 
@@ -27,6 +30,6 @@ server.use(function(err,req,res,text){
 
 
 
-server.listen(PORT , ()=>{
+server.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 });
